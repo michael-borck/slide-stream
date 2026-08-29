6e8b50fdf2859cf3788fb1d5cf0b3e6ef65f0168
Michael Borck <michael@borck.dev>
2026-08-29 18:36:53 +0800

    chore: release 2.15.18 (concurrent image + notes per slide)

--- src/slide_stream/__init__.py
+++ src/slide_stream/__init__.py
@@ -1,5 +1,5 @@
 """Slide Stream - AI-powered tool for creating video presentations from Markdown."""
 
-__version__ = "2.15.17"
+__version__ = "2.15.18"
 __author__ = "Michael Borck"
 __email__ = "michael.borck@curtin.edu.au"
